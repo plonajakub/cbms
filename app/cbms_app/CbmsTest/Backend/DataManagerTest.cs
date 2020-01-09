feeb5e3940dabf974616bc0c09772b3fb6823f2b
Jakub Plona <plonajakub@gmail.com>
2020-01-09 22:25:53 +0100

    Add test project

---
+++ app/cbms_app/CbmsTest/Backend/DataManagerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CbmsTest.Backend
+{
+    [TestClass]
+    public class DataManagerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
