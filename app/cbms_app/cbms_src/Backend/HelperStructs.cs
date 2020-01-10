532a5a56782f1f04951d8b6312c5699a0db5d1ef
Jakub Plona <plonajakub@gmail.com>
2020-01-10 13:03:49 +0100

    Add FundsPackState as set of readonly properties in HelperStructs

--- app/cbms_app/cbms_src/Backend/HelperStructs.cs
+++ app/cbms_app/cbms_src/Backend/HelperStructs.cs
@@ -5,5 +5,11 @@
         public static string In => "in";
         public static string Out => "out";
     };
+
+    public struct FundsPackState
+    {
+        public static string Add => "add";
+        public static string Fin => "fin";
+    };
     
 }
