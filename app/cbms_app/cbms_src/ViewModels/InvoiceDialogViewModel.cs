005e85bb0bd6a8e3fac48040e78d2863f1f4784d
Szymon <241309@student.pwr.edu.pl>
2020-01-14 17:54:38 +0100

    Some magic with products

--- app/cbms_app/cbms_src/ViewModels/InvoiceDialogViewModel.cs
+++ app/cbms_app/cbms_src/ViewModels/InvoiceDialogViewModel.cs
@@ -1,12 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
+using CbmsSrc.Backend;
 
 namespace CbmsSrc.ViewModels
 {
-    class InvoiceDialogViewModel
+    class InvoiceDialogViewModel : Screen
     {
+        private BindableCollection<Product> products;
+        private Dictionary<Category, List<Product>> products_categories;
+        private BindableCollection<Category> categories;
+        private Category selectedCategory;
+        public BindableCollection<Product> Products
+        {
+            get { return products; }
+            set { products = value; }
+        }
+
+        public Category SelectedCategory
+        {
+            get { return selectedCategory; }
+            set
+            {
+                selectedCategory = value;
+                NotifyOfPropertyChange(() => SelectedCategory);
+            }
+        }
+
+        private DataService dataService;
+        public InvoiceDialogViewModel()
+        {
+            dataService = new DataService();
+            products_categories = dataService.GetAllProducts();
+
+            products = new BindableCollection<Product>();
+            categories = new BindableCollection<Category>();
+            foreach (var category in products_categories)
+            {
+                categories.Add(category.Key);
+            }
+
+            SelectedCategory = categories.First();
+
+        }
     }
 }
