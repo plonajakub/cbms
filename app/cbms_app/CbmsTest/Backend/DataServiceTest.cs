ea71a457011019c2b7d1c70b0bdc37dc50fc2565
Jakub Plona <plonajakub@gmail.com>
2020-01-14 18:22:27 +0100

    Rewrite tests

--- app/cbms_app/CbmsTest/Backend/DataServiceTest.cs
+++ app/cbms_app/CbmsTest/Backend/DataServiceTest.cs
@@ -11,6 +11,11 @@ namespace CbmsTest.Backend
     [TestClass]
     public class DataServiceTest
     {
+        private DataService _dataService;
+
+        [TestInitialize]
+        public void ReinitializeDataService() => _dataService = new DataService();
+
         [TestMethod]
         public void Test_Mock_AddInvoice()
         {
@@ -35,28 +40,11 @@ namespace CbmsTest.Backend
         }
 
 
-        // TODO Test_Mock_UpdateInvoice not working
         [TestMethod]
-        public void Test_Mock_UpdateInvoice()
+        public void Test_GetAllProducts()
         {
-            var mockSet = new Mock<DbSet<Invoice>>();
-
-            var mockContext = new Mock<CbmsMainDbEntities>();
-            mockContext.Setup(m => m.Invoices).Returns(mockSet.Object);
-
-            var service = new DataService(mockContext.Object);
-            service.UpdateInvoice(new Invoice()
-            {
-                BusinessPartnerID = 1,
-                DepartmentID = 2,
-                Number = 3,
-                Type = InvoiceType.In,
-                IssueDate = DateTime.Now
-            });
-            service.SaveToDb();
-
-            mockSet.Verify(m => m.First(It.IsAny<Func<Invoice, bool>>()), Times.Once());
-            mockContext.Verify(m => m.SaveChanges(), Times.Once());
+            var allProducts = _dataService.GetAllProducts();
+            Assert.IsNotNull(allProducts);
         }
     }
 }
